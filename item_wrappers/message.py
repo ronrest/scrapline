848148ecc074acc0f0d065a68ee5cae9cd20b428
ronrest <ronny.coding@gmail.com>
2018-05-24 22:45:41 +1000

    CHORE: remove un-needed imports in message

--- item_wrappers/message.py
+++ item_wrappers/message.py
@@ -1,9 +1,6 @@
-from support.misc import MapDict
-from support.misc import set_timezone, convert_timezone, datetime2str
-from support.misc import now_datetime
-from support.telegram import id_from_peerobject
 DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 
+# TODO: consider replacing this with protoItem
 class ProtoMessage(object):
     def __init__(self):
         self._contents = {}
