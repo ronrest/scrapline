c0dfb91fd68d6a58cd7df7aa0b74c9249306299c
ronrest <ronny.coding@gmail.com>
2018-05-25 16:58:34 +1000

    FEAT: Update reddit message

--- item_wrappers/reddit_message.py
+++ item_wrappers/reddit_message.py
@@ -1,18 +1,19 @@
 from . message import ProtoMessage
-from support.datetimefuncs import now_string, timestamp2str
+from .. support.datetimefuncs import now_string, timestamp2str
 
 
-class RedditMessage(ProtoMessage):
-    def __init__(self, message):
+class RedditComment(ProtoMessage):
+    def __init__(self, comment):
         ProtoMessage.__init__(self)
-        self._contents = {}
         self.date_format = "%Y-%m-%d %H:%M:%S"
         self.tz = "UTC"
+        self._contents = self.extract_contents(comment)
 
-    def extract_contents(self, message):
-        self._contents = {
+    def extract_contents(self, comment):
+        contents = {
             "id": comment.id,
-            "created_utc": timestamp2str(comment.created_utc, tz=self.tz, format=self.format), # TODO: convert to timestring
+            "created_utc": timestamp2str(comment.created_utc, tz=self.tz, format=self.date_format),
+            "retrieved_on": now_string(format=self.date_format, tz=self.tz),
             # "retrieved_on": now_string(format=self.date_format, tz=self.tz),      # TODO: import library to get timenow as string
             # "author": comment.author.id,
             "body": comment.body,
@@ -31,16 +32,16 @@ class RedditMessage(ProtoMessage):
             # "subreddit": comment.subreddit.display_name,
             "scrape_status": "new",
             }
-
-
+        return contents
+        
     def summary(self, format="", text_limit=20):
-        return format.format(text_short=self._contents["text"][:text_limit], **self._contents)
+        return format.format(text_short=self._contents["body"][:text_limit]+"...", **self._contents)
     @property
     def dict(self):
         return self._contents
     def asdict(self):
         return self._contents
     def __str__(self):
-        return self.summary()
+        return self.summary("{id} {text_short}")
     def __repr__(self):
-        return "<MESSAGE OBJECT:  {}>".format(self.summary())
+        return "<Reddit COMMENT:  {}>".format(self.__str__())
