3f0c08ce9ddc2470d33516926ca6cd230cb59b3f
ronrest <ronny.coding@gmail.com>
2018-05-27 20:32:13 +1000

    FEAT: Add WorkerGroup class

--- modules/workers.py
+++ modules/workers.py
@@ -83,3 +83,195 @@ class ProtoWorkerThread(threading.Thread):
 
 
 
+# ##############################################################################
+#                               WORKER GROUP
+# ##############################################################################
+class WorkerGroup(object):
+    """ Object that wraps around a list of thread workers to provide conveneince
+        functions about the health of the worker thread objects.
+
+    """
+    def __init__(self, workers=None, name="workers"):
+        self._workers = workers if workers is not None else list()
+        self.name = name
+
+    def start(self, i=None):
+        if i is None:
+            for worker in self._workers:
+                worker.start()
+        else:
+            assert False, "Starting a specific isolated thread Not implemented yet"
+
+    def stop(self, i=None, lock=False):
+        """ Sends a stop signal to the worker threads, to stop on their
+            next refresh cycle.
+        Args:
+            i:  (int | None)(default=None)
+                isolate a specific thread number to close.
+                `None` closes all threads.
+            lock: (bool)(default=False)
+                Should the current thread halt untill all worker threads
+                are actually stopped?
+        """
+        if i is None:
+            logger.info("STOPPING all storer worker threads in {}".format(self.name))
+            for worker in self._workers:
+                worker.stop()
+            if lock:
+                for worker in self._workers:
+                    worker.join()
+                logger.debug("STOPPED all worker threads in {}".format(self.name))
+
+        else:
+            assert False, "Stopping a specific isolated thread not implemented yet"
+
+
+    def nullify(self, i=None):
+        """ Sets all (or selected worker) threads to None """
+        if i is None:
+            for i in range(self.n_workers):
+                self._workers[i] = None
+        else:
+            self._workers[i] = None
+
+    def clear(self):
+        """ Empties out the threads list """
+        self._workers = list()
+
+    @property
+    def n_workers(self):
+        return len(self._workers)
+
+    @property
+    def status(self):
+        return [getattr(worker, "status", None) for worker in self._workers]
+
+    @property
+    def is_running(self):
+        return any([status=="running" for status in self.status])
+
+    @property
+    def all_finished(self):
+        """ returns """
+        return all([(status=="success" or status=="fail") for status in self.status]) and self.n_workers > 0
+
+    @property
+    def all_success(self):
+        return all([status=="success" for status in self.status]) and self.n_workers > 0
+
+    @property
+    def has_failures(self):
+        return any([status=="fail" for status in self.status])
+
+    @property
+    def all_failures(self):
+        return all([status=="fail" for status in self.status])
+
+    @property
+    def has_nones(self):
+        return any([status is None for status in self.status])
+
+    @property
+    def alive_status(self):
+        return [worker.is_alive() for worker in self._workers]
+
+    @property
+    def has_dead_threads(self):
+        return any([not worker.is_alive() for worker in self._workers])
+
+    @property
+    def all_threads_alive(self):
+        return all([worker.is_alive() for worker in self._workers])
+
+    def __str__(self):
+        return str(self._workers)
+
+    def __repr__(self):
+        return '<WorkerGroup: ("{}" {})>'.format(self.name, str(self._workers))
+
+    def __len__(self):
+        return len(self._workers)
+
+    def append(self, item):
+        self._workers.append(item)
+
+    def remove(self, item):
+        self._workers.remove(item)
+
+    def __getitem__(self, sliced):
+        return self._workers[sliced]
+
+    def __setitem__(self, sliced, value):
+        self._workers[sliced] = value
+
+
+
+# from support.threads import WorkerGroup
+class WorkerGroupThreadListWrapper(WorkerGroup):
+    """ Tries to provide a workergroup interface to a list of ordinary thread
+        objects that are not WorkerThread class
+    """
+    def __init__(self, threads, name="workers"):
+        WorkerGroup.__init__(self, workers=threads, name=name)
+        # self.workers =
+
+    def worker_status(self, i):
+        worker = self._workers[i]
+        if hasattr(worker, "status"):
+            return worker.status
+        elif worker.isAlive():
+            return "running"
+        # elif hasattr(worker, "status") and worker.status=="fail":
+        #     return "fail"
+        # elif hasattr(worker, "status") and worker.status=="success":
+        #     return "success"
+        else:
+            return None
+
+    @property
+    def status(self):
+        return [self.worker_status(i) for i in range(self.n_workers)]
+
+    @property
+    def overall_status(self):
+        # return [getattr(worker, "status", None) for worker in self._workers]
+        if self.is_running:
+            return "running"
+        elif self.all_failures:
+            return "fail"
+        elif self.all_finished:
+            return "success"
+        else:
+            return None
+
+    @property
+    def is_running(self):
+        return any([worker.is_alive() for worker in self._workers])
+
+    @property
+    def all_finished(self):
+        assert False, "all_finished not implemented"
+        # return any([worker.is_alive() for worker in self._workers])
+
+    @property
+    def has_failures(self):
+        # TODO: look for status property in thread
+        assert False, "has_failures not implemented"
+
+    def start(self):
+        logger.warning("start() function in ClientWorkerGroupWrapper does nothing\n         other than set each thread status property to 'running'")
+        for i in range(self.n_workers):
+            self._workers[i].status = "running"
+
+    def stop(self, i=None, lock=False):
+        if lock:
+            logger.warning("`lock` argument in WorkerGroupThreadListWrapper is not yet operational")
+        if i is None:
+            for i in range(self.n_workers):
+                self._workers[i]._stop = True
+                self._workers[i].status = "success"
+                logger.warning("`_stop_thread` signal sent to all threads")
+        else:
+            self._workers[i]._stop_thread = True
+            self._workers[i].status = "success"
+            logger.warning("`_stop_thread` signal sent to thread {}".format(self._workers[i].name))
